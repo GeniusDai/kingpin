81c37afa3ba2e97646cadb1d178bef818c22daf8
hao <dev@localhost.localdomain>
2021-11-30 20:46:44 +0800

    add simple server

---
+++ examples/quick_start/src/test_server.cc
@@ -0,0 +1,23 @@
+#include "kingpin/IOHandler.h"
+#include "kingpin/ThreadSharedData.h"
+#include "kingpin/EpollTP.h"
+
+#include <cstring>
+
+template<typename _Data>
+class SimpleHandler : public IOHandlerForServer<_Data> {
+public:
+    SimpleHandler(_Data *d) : IOHandlerForServer<_Data>(d) {}
+    void onConnect(int conn) {
+        const char *str = "Hello, kingpin!";
+        ::write(conn, str, strlen(str));
+        ::close(conn);
+    }
+};
+
+int main() {
+    ServerTPSharedData data;
+    EpollTPServer<SimpleHandler, ServerTPSharedData> server(8, 8888, &data);
+    server.run();
+    return 0;
+}
