ccc475ac542893dea6c0619227bade1fded0d3c3
hao <dev@localhost.localdomain>
2021-12-03 00:34:25 +0800

    add new ut

---
+++ examples/quick_start/src/test_buffer.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <unistd.h>
+#include <cstring>
+#include <thread>
+#include <ctime>
+
+#include "kingpin/Logger.h"
+
+using namespace std;
+
+int main() {
+    Buffer buffer;
+    buffer.appendToBuffer("hello\n");
+    cout << buffer.endsWith("o\n") << endl;
+    cout << buffer.endsWith("\n") << endl;
+    cout << buffer.endsWith("\t") << endl;
+    return 0;
+}
