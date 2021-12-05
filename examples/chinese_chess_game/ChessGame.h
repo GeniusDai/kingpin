57a99984919954c2fabe2bb49060f21b7885484d
hao <dev@localhost.localdomain>
2021-12-05 22:36:03 +0800

    update chessgame

--- examples/chinese_chess_game/ChessGame.h
+++ examples/chinese_chess_game/ChessGame.h
@@ -1,3 +1,6 @@
+#ifndef _ChessGame_H_de485c8f1246_
+#define _ChessGame_H_de485c8f1246_
+
 #include <iostream>
 #include <vector>
 #include <memory>
@@ -8,98 +11,72 @@
 
 using namespace std;
 
-#ifndef _ChessGame_
-#define _ChessGame_
-
 class ChessGame {
 public:
-    enum class Player{
-        RED = 1,
-        BLACK,
-    };
+    enum class Player { RED = 1, BLACK };
 
-    enum class ChessType {
-        ROOK = 1,
-        KNIGHT,
-        ELEPHANT,
-        MANDARIN,
-        KING,
-        CANNON,
-        PAWN,
-    };
+    enum class ChessType { ROOK = 1, KNIGHT, ELEPHANT, MANDARIN, KING, CANNON, PAWN };
 
     struct Chess {
-        Chess(ChessType t, Player p) : t_(t), p_(p) {}
-        ChessType t_;
-        Player p_;
-        bool operator==(const Chess &chess) const {
-            return t_ == chess.t_ && p_ == chess.p_;
-        }
-    };
+        Chess(ChessType t, Player p) : _t(t), _p(p) {}
+        ChessType _t;
+        Player _p;
 
-    struct ChessHash {
-        size_t operator()(const Chess &chess) const {
-            return (static_cast<size_t>(chess.t_) * 10 + static_cast<size_t>(chess.p_)) / 19;
+        struct ChessHash {
+            size_t operator()(const Chess &chess) const {
+                return static_cast<size_t>(chess._t) * 10 +
+                    static_cast<size_t>(chess._p) / 19;
+            }
+        };
+
+        bool operator==(const Chess &chess) const {
+            return _t == chess._t && _p == chess._p;
         }
     };
 
     struct EnumHash {
         template <typename T>
-        size_t operator()(const T &t) const {
-            return static_cast<size_t>(t);
-        }
+        size_t operator()(const T &t) const { return static_cast<size_t>(t); }
     };
 
+    typedef shared_ptr<Chess> _chess_ptr;
+
+    static const string RED_COLOR;
+    static const string BLACK_COLOR;
+    static const int COLUMNS = 9;
+    static const int ROWS = 10;
+    static unordered_map<Player, string, EnumHash> playerHash;
+    static unordered_map<Chess, string, Chess::ChessHash> chessHash;
+
+    vector<vector<_chess_ptr> > _board;
+
     ChessGame() {
-        initHash();
-        board_ = vector<vector<_chess_ptr> >(ROWS, vector<_chess_ptr>(COLUMNS, nullptr));
+        _board = vector<vector<_chess_ptr> >(ROWS, vector<_chess_ptr>(COLUMNS, nullptr));
         initRows(0, Player::RED); initRows(ROWS - 1, Player::BLACK);
         initRows(2, Player::RED); initRows(ROWS - 3, Player::BLACK);
         initRows(3, Player::RED); initRows(ROWS - 4, Player::BLACK);
     }
 
-    void initHash() {
-
-        playerHash[Player::RED] = RED_COLOR;
-        playerHash[Player::BLACK] = BLACK_COLOR;
-
-        chessHash[{ChessType::ROOK, Player::RED}] = "车";
-        chessHash[{ChessType::KNIGHT, Player::RED}] = "马";
-        chessHash[{ChessType::ELEPHANT, Player::RED}] = "象";
-        chessHash[{ChessType::MANDARIN, Player::RED}] = "士";
-        chessHash[{ChessType::KING, Player::RED}] = "帅";
-        chessHash[{ChessType::CANNON, Player::RED}] = "炮";
-        chessHash[{ChessType::PAWN, Player::RED}] = "兵";
-
-        chessHash[{ChessType::ROOK, Player::BLACK}] = "车";
-        chessHash[{ChessType::KNIGHT, Player::BLACK}] = "马";
-        chessHash[{ChessType::ELEPHANT, Player::BLACK}] = "象";
-        chessHash[{ChessType::MANDARIN, Player::BLACK}] = "仕";
-        chessHash[{ChessType::KING, Player::BLACK}] = "将";
-        chessHash[{ChessType::CANNON, Player::BLACK}] = "炮";
-        chessHash[{ChessType::PAWN, Player::BLACK}] = "卒";
-    }
-
     void initRows(int row, Player p) {
         if (row == 0 or row == ROWS - 1) {
-            board_[row][0] = _chess_ptr(new Chess(ChessType::ROOK, p));
-            board_[row][1] = _chess_ptr(new Chess(ChessType::KNIGHT, p));
-            board_[row][2] = _chess_ptr(new Chess(ChessType::ELEPHANT, p));
-            board_[row][3] = _chess_ptr(new Chess(ChessType::MANDARIN, p));
-            board_[row][4] = _chess_ptr(new Chess(ChessType::KING, p));
-            board_[row][COLUMNS - 4] = _chess_ptr(new Chess(ChessType::MANDARIN, p));
-            board_[row][COLUMNS - 3] = _chess_ptr(new Chess(ChessType::ELEPHANT, p));
-            board_[row][COLUMNS - 2] = _chess_ptr(new Chess(ChessType::KNIGHT, p));
-            board_[row][COLUMNS - 1] = _chess_ptr(new Chess(ChessType::ROOK, p));
+            _board[row][0] = _chess_ptr(new Chess(ChessType::ROOK, p));
+            _board[row][1] = _chess_ptr(new Chess(ChessType::KNIGHT, p));
+            _board[row][2] = _chess_ptr(new Chess(ChessType::ELEPHANT, p));
+            _board[row][3] = _chess_ptr(new Chess(ChessType::MANDARIN, p));
+            _board[row][4] = _chess_ptr(new Chess(ChessType::KING, p));
+            _board[row][COLUMNS - 4] = _chess_ptr(new Chess(ChessType::MANDARIN, p));
+            _board[row][COLUMNS - 3] = _chess_ptr(new Chess(ChessType::ELEPHANT, p));
+            _board[row][COLUMNS - 2] = _chess_ptr(new Chess(ChessType::KNIGHT, p));
+            _board[row][COLUMNS - 1] = _chess_ptr(new Chess(ChessType::ROOK, p));
         } else if (row == 2 or row == ROWS - 3) {
-            board_[row][1] = _chess_ptr(new Chess(ChessType::CANNON, p));
-            board_[row][COLUMNS - 2] = _chess_ptr(new Chess(ChessType::CANNON, p));
+            _board[row][1] = _chess_ptr(new Chess(ChessType::CANNON, p));
+            _board[row][COLUMNS - 2] = _chess_ptr(new Chess(ChessType::CANNON, p));
         } else if (row == 3 or row == ROWS - 4) {
-            board_[row][0] = _chess_ptr(new Chess(ChessType::PAWN, p));
-            board_[row][2] = _chess_ptr(new Chess(ChessType::PAWN, p));
-            board_[row][4] = _chess_ptr(new Chess(ChessType::PAWN, p));
-            board_[row][COLUMNS - 3] = _chess_ptr(new Chess(ChessType::PAWN, p));
-            board_[row][COLUMNS - 1] = _chess_ptr(new Chess(ChessType::PAWN, p));
+            _board[row][0] = _chess_ptr(new Chess(ChessType::PAWN, p));
+            _board[row][2] = _chess_ptr(new Chess(ChessType::PAWN, p));
+            _board[row][4] = _chess_ptr(new Chess(ChessType::PAWN, p));
+            _board[row][COLUMNS - 3] = _chess_ptr(new Chess(ChessType::PAWN, p));
+            _board[row][COLUMNS - 1] = _chess_ptr(new Chess(ChessType::PAWN, p));
         }
     }
 
@@ -108,10 +85,10 @@ public:
         for (int i = 0; i < ROWS; ++i) {
             cout << i << " ";
             for (int j = 0; j < COLUMNS; ++j) {
-                _chess_ptr tmpChess(board_[i][j]);
+                _chess_ptr tmpChess(_board[i][j]);
                 if (tmpChess == nullptr) { cout << " " << " " << " "; }
                 else {
-                    cout << playerHash[tmpChess->p_] << chessHash[*tmpChess];
+                    cout << playerHash[tmpChess->_p] << chessHash[*tmpChess];
                     cout << "\033[0m" << " ";
                 }
             }
@@ -124,8 +101,8 @@ public:
 
     void moveChess(int fx, int fy, int tx, int ty) {
         if (fx == tx && fy == ty) return;
-        board_[tx][ty] = board_[fx][fy];
-        board_[fx][fy] = nullptr;
+        _board[tx][ty] = _board[fx][fy];
+        _board[fx][fy] = nullptr;
     }
 
     void moveChess(string move) {
@@ -153,19 +130,33 @@ public:
         }
         return data;
     }
-
-private:
-    static const string RED_COLOR;
-    static const string BLACK_COLOR;
-    typedef shared_ptr<Chess> _chess_ptr;
-    vector<vector<_chess_ptr> > board_;
-    static const int COLUMNS = 9;
-    static const int ROWS = 10;
-    unordered_map<Player, string, EnumHash> playerHash;
-    unordered_map<Chess, string, ChessHash> chessHash;
 };
 
 const string ChessGame::RED_COLOR = "\033[47;31m";
 const string ChessGame::BLACK_COLOR = "\033[47;30m";
+
+unordered_map<ChessGame::Player, string, ChessGame::EnumHash>
+ChessGame::playerHash = {
+    { ChessGame::Player::RED, ChessGame::RED_COLOR},
+    { ChessGame::Player::BLACK, ChessGame::BLACK_COLOR}
+};
+
+unordered_map<ChessGame::Chess, string, ChessGame::Chess::ChessHash>
+ChessGame::chessHash = {
+    {{ChessGame::ChessType::ROOK, ChessGame::Player::RED}, "车"},
+    {{ChessGame::ChessType::KNIGHT, ChessGame::Player::RED}, "马"},
+    {{ChessGame::ChessType::ELEPHANT, ChessGame::Player::RED}, "象"},
+    {{ChessGame::ChessType::MANDARIN, ChessGame::Player::RED}, "士"},
+    {{ChessGame::ChessType::KING, ChessGame::Player::RED}, "帅"},
+    {{ChessGame::ChessType::CANNON, ChessGame::Player::RED}, "炮"},
+    {{ChessGame::ChessType::PAWN, ChessGame::Player::RED}, "兵"},
+    {{ChessGame::ChessType::ROOK, ChessGame::Player::BLACK}, "车"},
+    {{ChessGame::ChessType::KNIGHT, ChessGame::Player::BLACK}, "马"},
+    {{ChessGame::ChessType::ELEPHANT, ChessGame::Player::BLACK}, "象"},
+    {{ChessGame::ChessType::MANDARIN, ChessGame::Player::BLACK}, "仕"},
+    {{ChessGame::ChessType::KING, ChessGame::Player::BLACK}, "将"},
+    {{ChessGame::ChessType::CANNON, ChessGame::Player::BLACK}, "炮"},
+    {{ChessGame::ChessType::PAWN, ChessGame::Player::BLACK}, "卒"},
+};
 
 #endif // ChessGame.h
