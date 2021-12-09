f166b92a30133e097ebcdc6f449262d7d6abbbaf
hao <dev@localhost.localdomain>
2021-12-09 20:13:03 +0800

    use pthread to test concurrently

--- examples/unittest/test_utils_connect.cc
+++ examples/unittest/test_utils_connect.cc
@@ -1,26 +1,39 @@
 #include "kingpin/Utils.h"
+#include "kingpin/Exception.h"
 #include <iostream>
 #include <cstring>
 #include <unistd.h>
 #include <cstdio>
+#include <thread>
+#include <vector>
 
 using namespace std;
 using namespace kingpin;
 
-int main() {
+void func() {
     int sock;
+    try {
+        sock = connectHost("localhost", 9000, 10);
+    } catch (const TimeoutException &e) {
+        cout << e.what() << endl;
+        return;
+    }
+    cout << "connect complete" << endl;
+    const char *str = "hello, kingpin";
+    char buf[1];
+    if (::write(sock, str, ::strlen(str)) < 0) {
+        ::perror("syscall write error");
+    }
+}
+
+int main() {
+    vector<thread> vt;
     for (int i = 0; i < 100; ++i) {
         cout << "round " << i << endl;
-        sock = connectHost("localhost", 9000, 30);
-        const char *str = "hello, kingpin";
-        char buf[1];
-        if (::write(sock, str, ::strlen(str)) < 0) {
-            perror("syscall write error");
-        }
-        cout << "write complete" << endl;
-        if (::read(sock, buf, 1) < 0) {
-            continue;
-        }
+        vt.emplace_back(func);
+    }
+    for (int i = 0; i < vt.size(); ++i) {
+        vt[i].join();
     }
     return 0;
 }
